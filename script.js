596f05ec5066af152ff7fa3ae3ee22990e844c76
MonkeyDLuz <89729491+MonkeyDLuz@users.noreply.github.com>
2024-08-02 22:15:01 +0200

    Diverse improvements in all the pages

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+  AOS.init({
+      // You can add global settings here
+      duration: 800,
+      easing: 'ease-in-out',
+      once: true,
+
+  });
+});
+
+// You can add other JavaScript functions or event listeners here
