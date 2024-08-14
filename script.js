d69a30b7f9e265a1efdd9db02e8e7e383a20af0a
MonkeyDLuz <89729491+MonkeyDLuz@users.noreply.github.com>
2024-08-14 20:48:13 +0200

    Multiple changes

--- script.js
+++ script.js
@@ -1,11 +1,38 @@
-document.addEventListener('DOMContentLoaded', function() {
-  AOS.init({
-      // You can add global settings here
-      duration: 800,
-      easing: 'ease-in-out',
-      once: true,
-
-  });
+document.addEventListener("DOMContentLoaded", function () {
+	AOS.init({
+		// You can add global settings here
+		duration: 800,
+		easing: "ease-in-out",
+		once: true,
+	});
 });
 
 // You can add other JavaScript functions or event listeners here
+
+document.getElementById("emailButton").addEventListener("click", function (e) {
+	e.preventDefault();
+	const email = "example@example.com"; // Replace with the predetermined email address
+	const subject = "Contact Request"; // Replace with your subject
+
+	// Get the values from the form
+	const name = document.getElementById("name").value;
+	const userEmail = document.getElementById("email").value;
+	const phone = document.getElementById("phone").value;
+	const message = document.getElementById("message").value;
+
+	let body;
+
+	if (message.trim() !== "") {
+		// If there's a message in the form, use it
+		body = `Name: ${name}\nEmail: ${userEmail}\nPhone: ${phone}\n\nMessage:\n${message}`;
+	} else {
+		// If no message, use the predetermined text
+		body = `Name: ${name}\nEmail: ${userEmail}\nPhone: ${phone}\n,\n\nI would like to get in touch with you.`;
+	}
+
+	const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+		subject
+	)}&body=${encodeURIComponent(body)}`;
+
+	window.location.href = mailtoLink;
+});
